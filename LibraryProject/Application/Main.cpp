6c9baedd87f42eb537bf5be164992a5142dec753
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2017-03-05 22:06:16 +0900

    [enh #2]Windowクラスの実装とテスト駆動
    -Windowクラス実装
    -テスト駆動とソース修正

--- LibraryProject/Application/Main.cpp
+++ LibraryProject/Application/Main.cpp
@@ -1,10 +1,44 @@
-//==================================================================================================================================//
+﻿//==================================================================================================================================//
 //!< @file		Main.cpp
-//!< @brief		Main.cpp
+//!< @brief		Library用テストプログラム
 //!< @author	T.Haga
 //==================================================================================================================================//
 
+/** @todo このソースはもう少しLibraryができてからきれいにする */
 
+#include <windows.h>
+#include "sl/sl.h"
+
+#define TITLE "テスト"
+#define WIN_WIDTH	1200
+#define WIN_HEIGHT	900
+
+/* Entry Point ---------------------------------------------------------------------------------------- */
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow)
+{
+	sl::IWindow*	pWindow = nullptr;
+	pWindow = new sl::dx11::Window();
+
+	if(!pWindow->Initalize(TITLE, WIN_WIDTH, WIN_HEIGHT))
+	{
+		// 作成失敗
+	}
+	while(true)
+	{
+		if(pWindow->Update())
+		{
+			break;
+		}
+		else
+		{
+
+		}
+	}
+
+	delete pWindow;
+	return 0;
+}
 
 //==================================================================================================================================//
 //END OF FILE
