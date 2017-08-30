ddc835a27351f0cef38d75f124e808dc9c92d0a2
Sharoku-haga <beker221nob@yahoo.co.jp>
2017-08-30 14:09:12 +0900

    [ ref #25] ライブラリの整理と初期化
    -ライブラリの初期化
    -ソースの整理

--- LibraryProject/Application/App/App.cpp
+++ LibraryProject/Application/App/App.cpp
@@ -15,91 +15,20 @@ namespace app
 
 /* Public Functions ------------------------------------------------------------------------------------------- */
 
-App::App(void)
-	: m_pLibrary(sl::ISharokuLibrary::Instance())
-	, m_IsEnd(false)
-	, m_Pos({0.0f, 0.0f})
+App::App()
 {}
 
-App::~App(void)
-{
-	Finalize();
-}
+App::~App()
+{}
 
 void App::Initialize()
-{
-	m_IDs.m_TexID = m_pLibrary->LoadTexture("rinne.png");
-	//m_IDs.m_TexID = m_pLibrary->LoadTexture("BLUE.png");
+{}
 
-	sl::fRect size = {0.0f, 0.0f, 300.f, 300.f};
-	sl::fRect uv = {0.0f, 0.0f, 1.0f, 1.0f};
+void App::Finalize()
+{}
 
-	m_IDs.m_VtxID= m_pLibrary->CreateVertex2D(size, uv);
-
-	m_pLibrary->RegisterCustomizeType(UP, sl::GAMEPAD, sl::XIGAMEPAD_LSTICK_UP);
-	m_pLibrary->RegisterCustomizeType(DOWN, sl::GAMEPAD, sl::XIGAMEPAD_LSTICK_DOWN);
-	m_pLibrary->RegisterCustomizeType(RIGHT, sl::GAMEPAD, sl::XIGAMEPAD_LSTICK_RIGHT);
-	m_pLibrary->RegisterCustomizeType(LEFT, sl::GAMEPAD, sl::XIGAMEPAD_LSTICK_LEFT);
-	m_pLibrary->RegisterCustomizeType(ATTACK, sl::GAMEPAD, sl::XIGAMEPAD_A);
-	m_pLibrary->RegisterCustomizeType(JUMP, sl::GAMEPAD, sl::XIGAMEPAD_B);
-}
-
-void App::Finalize(void)
-{
-	m_pLibrary->ReleaseVertexALL();
-	m_pLibrary->ReleaseTexALL();
-}
-
-bool App::Update(void)
-{
-	m_pLibrary->UpdateInputDevice();
-
-	Control();
-
-	m_pLibrary->StartRender();
-
-	Draw();
-
-	m_pLibrary->EndRender();
-
-	return m_IsEnd;
-}
-
-/* Private Functions ------------------------------------------------------------------------------------------ */
-
-void App::Control(void)
-{
-	if(m_pLibrary->CheckCustomizeState(RIGHT) == sl::ON)
-	{
-		m_Pos.x += 10.0f;
-	}
-	if(m_pLibrary->CheckCustomizeState(LEFT) == sl::ON)
-	{
-		m_Pos.x -= 10.0f;
-	}
-	if(m_pLibrary->CheckCustomizeState(UP) == sl::ON)
-	{
-		m_Pos.y -= 10.0f;
-	}
-	if(m_pLibrary->CheckCustomizeState(DOWN) == sl::ON)
-	{
-		m_Pos.y += 10.0f;
-	}
-
-	if(m_pLibrary->CheckCustomizeState(ATTACK) == sl::ON)
-	{
-		m_Pos.x += 10.0f;
-	}
-	if(m_pLibrary->CheckCustomizeState(JUMP) == sl::ON)
-	{
-		m_Pos.x -= 10.0f;
-	}
-}
-
-void App::Draw(void)
-{
-	m_pLibrary->Draw2D(m_IDs, m_Pos);
-}
+bool App::Update()
+{}
 
 }	// namespace app
 
