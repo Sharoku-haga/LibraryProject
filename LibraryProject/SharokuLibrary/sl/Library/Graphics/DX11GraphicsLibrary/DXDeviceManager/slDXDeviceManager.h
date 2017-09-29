420a690d6d701249539d09389db2a058d50b744f
Sharoku-haga <beker221nob@yahoo.co.jp>
2017-09-29 10:34:14 +0900

    [enh #45] DXDeviceManagerクラスの作成と実装
    -DXDeviceManagerクラスの作成と実装
    -テスト完了とテスト結果にもとづくソース修正

---
+++ LibraryProject/SharokuLibrary/sl/Library/Graphics/DX11GraphicsLibrary/DXDeviceManager/slDXDeviceManager.h
@@ -0,0 +1,57 @@
+﻿//==================================================================================================================================//
+//!< @file		slDXDeviceManager.h
+//!< @brief		sl::DXDeviceManagerクラスのヘッダ
+//!< @author	T.Haga
+//!< @data		作成日時：2017/09/29	更新履歴：
+//==================================================================================================================================//
+
+#ifndef SL_DX_DEVICE_MANAGER_H
+#define SL_DX_DEVICE_MANAGER_H
+
+#include <windows.h>
+#include <d3dx11.h>
+#include "../../../Utility/slDefine.h"
+
+namespace sl
+{
+
+//===================================================================================//
+//!< DirectXのデバイスを管理するクラス
+//===================================================================================//
+class DXDeviceManager
+{
+
+public:
+	/** Constructor */
+	DXDeviceManager();
+
+	/** Destructor */
+	~DXDeviceManager();
+
+	/**
+	* デバイスを生成する関数 
+	* @return true→成功 false→失敗
+	*/
+	bool CreateDevice();
+
+	/**
+	* デバイスを取得する関数
+	* @return DirectX11のデバイス
+	*/
+	inline ID3D11Device*		GetDevice(void) const { return m_pDevice;  }
+
+private:
+	ID3D11Device*					m_pDevice;					//!< Direct3Dのデバイス
+
+	/** コピー禁止 */
+	SL_DISALLOW_COPY_AND_ASSIGN(DXDeviceManager);
+
+};	// class DXDeviceManager
+
+}	// namespace sl
+
+#endif	// SL_DX_DEVICE_MANAGER_H
+
+//==================================================================================================================================//
+// END OF FILE
+//==================================================================================================================================//
